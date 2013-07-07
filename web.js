e5a61ea2bcfbcccdfb997731742d19fbe41f493c
prasad parigi <prasadparigi2@gmail.com>
2013-07-07 08:31:00 +0000

    Updated web.js

--- web.js
+++ web.js
@@ -1,9 +1,15 @@
+#!/usr/bin/env node
 var express = require('express');
 
 var app = express.createServer(express.logger());
-
+var fs = require('fs');
+fs.readFile('index.html', function (err, data) {
+  if (err) throw err;
+  str = data.toString();
+  console.log(data.toString());
+});
 app.get('/', function(request, response) {
-  response.send('Hello World!');
+    response.send(str);
 });
 
 var port = process.env.PORT || 5000;
